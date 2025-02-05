253d618fb8fffe1f24db2dc09cbebb011c5913e2
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-02-05 06:53:21 +0300

    Add al-zram
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

---
+++ al-zram.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env dash
+#
+# Copyright (C) 2025 Maria Lisina
+# SPDX-License-Identifier: Apache-2.0
+#
+# Alpine Linux zram
+
+if [ -n "${1}" ]
+then
+    __num_devices=${1}
+
+    if ! test ${__num_devices} -gt 0 > /dev/null 2>&1
+    then
+        echo "Invalid devices number"
+        exit 1
+    fi
+
+    shift
+fi
+
+if [ -n "${1}" ]
+then
+    __size=${1}
+
+    if ! test ${__size} -gt 0 > /dev/null 2>&1
+    then
+        echo "Invalid block size"
+        exit 1
+    fi
+
+    shift
+fi
+
+cat << EOF > /etc/conf.d/zram-init
+load_on_start=yes
+unload_on_stop=no
+num_devices=${__num_devices}
+EOF
+
+for __device in $(seq 0 $((__num_devices - 1)))
+do
+    printf "\ntype%u=%s\nsize%u=%u\nmaxs%u=%u\nalgo%u=%s\nlabl%u=%s\n" \
+        ${__device} "swap" \
+        ${__device} ${__size} \
+        ${__device} 1 \
+        ${__device} "zstd" \
+        ${__device} "zram" >> /etc/conf.d/zram-init
+done
