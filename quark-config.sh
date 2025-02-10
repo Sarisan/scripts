ca8b860f2a69a5cb2c30a7fdb947e2f4d7121fe8
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-02-10 05:26:24 +0300

    quark-config: Configure Quark Kernel completely
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- quark-config.sh
+++ quark-config.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2025 Maria Lisina
 # SPDX-License-Identifier: Apache-2.0
 #
-# Quark Kernel base defconfig based on Azure Linux 6.6
+# Quark Kernel defconfig based on Azure Linux 6.6
 
 set -e
 
@@ -60,6 +60,63 @@ __wsl2_required_configs="
     CONFIG_NETFILTER_XT_MATCH_TCPMSS
 "
 
+__wsl2_quark_configs="
+    CONFIG_KERNEL_ZSTD
+
+    CONFIG_ZRAM
+    CONFIG_ZRAM_BACKEND_ZSTD
+    CONFIG_ZRAM_WRITEBACK
+    CONFIG_ZRAM_MEMORY_TRACKING
+    CONFIG_ZRAM_MULTI_COMP
+    CONFIG_CRYPTO_ZSTD
+
+    CONFIG_HIDRAW
+    CONFIG_HID_BPF
+    CONFIG_USB_HIDDEV
+    CONFIG_USB
+    CONFIG_USB_STORAGE
+    CONFIG_USB_UAS
+    CONFIG_USBIP_CORE
+    CONFIG_USBIP_VHCI_HCD
+    CONFIG_USB_SERIAL
+
+    CONFIG_USB_SERIAL_CH341
+
+    CONFIG_EROFS_FS
+
+    CONFIG_INIT_STACK_ALL_ZERO
+
+    CONFIG_POSIX_MQUEUE
+    CONFIG_CGROUPS
+    CONFIG_MEMCG
+    CONFIG_CGROUP_SCHED
+    CONFIG_CGROUP_FREEZER
+    CONFIG_CPUSETS
+    CONFIG_CGROUP_DEVICE
+    CONFIG_CGROUP_CPUACCT
+    CONFIG_CGROUP_BPF
+    CONFIG_NAMESPACES
+    CONFIG_UTS_NS
+    CONFIG_IPC_NS
+    CONFIG_PID_NS
+    CONFIG_NET_NS
+    CONFIG_BRIDGE_NETFILTER
+    CONFIG_NF_NAT
+    CONFIG_NETFILTER_XT_MARK
+    CONFIG_NETFILTER_XT_NAT
+    CONFIG_NETFILTER_XT_TARGET_MASQUERADE
+    CONFIG_NETFILTER_XT_MATCH_ADDRTYPE
+    CONFIG_NETFILTER_XT_MATCH_CONNTRACK
+    CONFIG_NETFILTER_XT_MATCH_IPVS
+    CONFIG_IP_VS
+    CONFIG_BRIDGE
+    CONFIG_VETH
+    CONFIG_KEYS
+
+    CONFIG_ANDROID_BINDER_IPC
+    CONFIG_ANDROID_BINDERFS
+"
+
 for __arch in arm64 x86_64
 do
     if [ ${__arch} = arm64 ]
@@ -72,16 +129,16 @@ do
     rm -fr ../out-${__arch}
     mkdir ../out-${__arch}
     cp ../azure-${__arch} ../out-${__arch}/.config
-    make -j 16 ARCH=${__arch} O=../out-${__arch} CROSS_COMPILE=${_arch}-linux-gnu- olddefconfig
-    sed -i 's/CONFIG_LOCALVERSION=.*$/CONFIG_LOCALVERSION="-microsoft-quark-WSL2"/' ../out-${__arch}/.config
-    sed -i 's/CONFIG_SYSTEM_TRUSTED_KEYS=.*$//' ../out-${__arch}/.config
+    make -j 16 O=../out-${__arch} ARCH=${__arch} CROSS_COMPILE=${_arch}-linux-gnu- olddefconfig
+    sed -i 's/^CONFIG_LOCALVERSION=.*$/CONFIG_LOCALVERSION="-microsoft-quark-WSL2"/' ../out-${__arch}/.config
+    sed -i 's/^CONFIG_SYSTEM_TRUSTED_KEYS=.*$//' ../out-${__arch}/.config
 
     for __config in ${__wsl2_required_configs}
     do
         echo "${__config}=y" >> ../out-${__arch}/.config
     done
 
-    make -j 16 ARCH=${__arch} O=../out-${__arch} CROSS_COMPILE=${_arch}-linux-gnu- olddefconfig
+    make -j 16 O=../out-${__arch} ARCH=${__arch} CROSS_COMPILE=${_arch}-linux-gnu- olddefconfig
 
     echo "# CONFIG_MODULES is not set" >> ../out-${__arch}/.config
 
@@ -90,6 +147,13 @@ do
         sed -i "s/${__module}/# ${__module%=*} is not set/" ../out-${__arch}/.config
     done
 
-    make -j 16 ARCH=${__arch} O=../out-${__arch} CROSS_COMPILE=${_arch}-linux-gnu- olddefconfig
-    make -j 16 ARCH=${__arch} O=../out-${__arch} CROSS_COMPILE=${_arch}-linux-gnu- savedefconfig
+    make -j 16 O=../out-${__arch} ARCH=${__arch} CROSS_COMPILE=${_arch}-linux-gnu- olddefconfig
+
+    for __config in ${__wsl2_quark_configs}
+    do
+        echo "${__config}=y" >> ../out-${__arch}/.config
+    done
+
+    make -j 16 O=../out-${__arch} ARCH=${__arch} CROSS_COMPILE=${_arch}-linux-gnu- olddefconfig
+    make -j 16 O=../out-${__arch} ARCH=${__arch} CROSS_COMPILE=${_arch}-linux-gnu- savedefconfig
 done
