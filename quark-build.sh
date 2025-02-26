d74dc361f347e29c4733e9e2f547546fd78503f8
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-02-26 12:29:54 +0300

    Add quark-build
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

---
+++ quark-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env dash
+#
+# Copyright (C) 2025 Maria Lisina
+# SPDX-License-Identifier: Apache-2.0
+#
+# Quark Kernel build script
+
+set -e
+
+for __arch in arm64 x86_64
+do
+    if [ ${__arch} = arm64 ]
+    then
+        _arch=aarch64
+    else
+        _arch=x86_64
+    fi
+
+    rm -fr ../out-${__arch}
+    make -j 16 O=../out-${__arch} ARCH=${__arch} CROSS_COMPILE=${_arch}-linux-gnu- wsl2_defconfig
+    make -j 16 O=../out-${__arch} ARCH=${__arch} CROSS_COMPILE=${_arch}-linux-gnu-
+done
