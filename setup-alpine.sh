cc669aa9ac61df16e2d14b2329b8745d2a54db96
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-06-08 23:38:26 +0500

    setup-alpine: Fix hostname
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- setup-alpine.sh
+++ setup-alpine.sh
@@ -11,9 +11,9 @@ __dir="${0%/*}"
 
 ${__dir}/setup-world.sh
 ${__dir}/fstab.sh ${1}
-${__dir}/networking.sh
+${__dir}/networking.sh ${2}
 ${__dir}/nft-accept-filter.sh
-${__dir}/rc-zram-init.sh ${2} ${3}
+${__dir}/rc-zram-init.sh ${3} ${4}
 ${__dir}/locale.sh
 ${__dir}/timezone.sh
 ${__dir}/squid.sh
