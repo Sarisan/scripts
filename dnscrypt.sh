fe23ec2c6b8f4a3332b797fe53089ac846277804
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-07-09 00:14:07 +0500

    dnscrypt: Use Yandex DNS only
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- dnscrypt.sh
+++ dnscrypt.sh
@@ -6,16 +6,10 @@
 # dnscrypt-proxy setup script
 
 sed -i \
-    -e "s/^listen_addresses =.*$/listen_addresses = [':53']/" \
+    -e "s/^# server_names =.*$/server_names = ['yandex', 'yandex-ipv6']/" \
+    -e "s/^listen_addresses =.*$/listen_addresses = ['127.0.0.1:53', '[::1]:53']/" \
     -e 's/^ipv6_servers =.*$/ipv6_servers = true/' \
     /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-
-__line_num=$(grep -n "\[sources.dnscry-pt-resolvers\]" /etc/dnscrypt-proxy/dnscrypt-proxy.toml | cut -d ':' -f 1)
-
-for _line_num in $(seq ${__line_num} $((__line_num + 5)))
-do
-    sed -i "${_line_num}s/^# //" /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-done
 
 cat << EOF > /etc/resolv.conf
 nameserver 127.0.0.1
