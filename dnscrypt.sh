bc0303a45f7ae8b70ea885b1260f0ae405077661
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-07-09 18:43:31 +0500

    dnscrypt: Disable cache
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- dnscrypt.sh
+++ dnscrypt.sh
@@ -9,6 +9,7 @@ sed -i \
     -e "s/^# server_names =.*$/server_names = ['yandex', 'yandex-ipv6']/" \
     -e "s/^listen_addresses =.*$/listen_addresses = ['127.0.0.1:53', '[::1]:53']/" \
     -e 's/^ipv6_servers =.*$/ipv6_servers = true/' \
+    -e 's/^cache =.*$/cache = false/' \
     /etc/dnscrypt-proxy/dnscrypt-proxy.toml
 
 cat << EOF > /etc/resolv.conf
