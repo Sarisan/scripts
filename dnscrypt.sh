e265b73a801d234a0f3eb26e60cc75597cdd436e
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-07-11 00:00:20 +0500

    dnscrypt: Use Google DNS
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- dnscrypt.sh
+++ dnscrypt.sh
@@ -6,7 +6,7 @@
 # dnscrypt-proxy setup script
 
 sed -i \
-    -e "s/^# server_names =.*$/server_names = ['yandex', 'yandex-ipv6']/" \
+    -e "s/^# server_names =.*$/server_names = ['google', 'google-ipv6']/" \
     -e "s/^listen_addresses =.*$/listen_addresses = ['127.0.0.1:53', '[::1]:53']/" \
     -e 's/^ipv6_servers =.*$/ipv6_servers = true/' \
     -e 's/^cache =.*$/cache = false/' \
